d8c42409b93fedf844c33855da12ef142f4ea166
Hari krishna <harikrishnacsk77@gmail.com>
2025-06-16 12:35:16 -0700

    changed api url path again

--- src/vendorDashbord/components/forms/Register.jsx
+++ src/vendorDashbord/components/forms/Register.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { API_URL } from '../../data/apiPath';
-import { Link } from 'react-router-dom'; // optional if you want to link to login page
+import { Link } from 'react-router-dom'; 
 
 const Register = ({ loginhandler }) => {
   const containerStyle = {
